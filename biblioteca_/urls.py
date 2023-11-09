cd1d1419c9130d739c681b2e6bfe3ae6f6dc86a5
Ghadrr <105183365+Ghadrr@users.noreply.github.com>
2023-11-09 20:55:35 -0300

    a
    asd

--- biblioteca_/urls.py
+++ biblioteca_/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('inicio/', views.index, name='home'),
+    path('', views.index, name='home'),
     path('product-detail/<int:id>/', views.livro_detail , name='product-detail')
 
 ]
